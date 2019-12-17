6a9cb1c5b4b423ade6832a94a04fb7dc6116d2a2
Subhra <das.subhra.k@gmail.com>
2019-12-17 21:46:29 +0530

    initServer modified!

--- initServer.js
+++ initServer.js
@@ -1,16 +1,23 @@
 const db2con=require('./dbCon');
 const express=require("express");
 const Promise=require("bluebird");
+const fs=require("fs");
+const fileread=Promise.promisify(fs.readFile);
+const filename=__dirname+"/def_emp.json";
 let data={};
 const app=new express();
 
 app.get('/',(req,res)=>{
-   console.log("no parameters provided!!!");
+    fileread(filename).then((chunk)=>{
+        data=JSON.parse(chunk);
+        res.send(data);
+    })
 })
 
 app.get('/:name',(req,res)=>{
     res.header("Access-Control-Allow-Origin", "*");
     let dbresult=db2con.connect(req.params.name);
+    res.send(JSON.stringify(dbresult));
 })
 
 app.listen(3000);
