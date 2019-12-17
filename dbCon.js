c946c7829094807c39ffc65772a786686374869c
Subhra <das.subhra.k@gmail.com>
2019-12-17 12:02:28 +0530

    initServer updated

--- dbCon.js
+++ dbCon.js
@@ -14,6 +14,7 @@ connection.connect(function(err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
+  // console.clear();
   console.log('Database Connected');
 });
 let value = matcher;
@@ -21,30 +22,12 @@ let queryst= `select * from emp where name='${value}'`;
 connection.query(queryst, function (err, data) {
      if (err) console.log(err);
      else {
-     console.log(data);
-     // result = data;
+     console.table(data);
+     result = data;
      }
 });
 connection.end(function () {       
     console.log('connection Closed');
 });
-
-// ibmdb.open("DATABASE=TEST;HOSTNAME=localhost;UID=ROUNAK;PWD=Farcry@5;PORT=50000;PROTOCOL=TCPIP", function (err,conn) {
-//   if (err) return console.log(err);
-//   let value=matcher;
-//   let queryst=`select * from CROUTINE as CROUTINE where ='${value}'`;
-//   conn.query(queryst, function (err, data) {
-//     if (err) console.log(err);
-//     else {
-//     console.log(data);
-//     result=data;
-//     }
-//     conn.close(function () {
-//       console.log('connection closed');
-//     });
-//   });
-// });
-// console.log(result);
-// return result;
 }
 module.exports.result = result;
--- initServer.js
+++ initServer.js
@@ -1,30 +1,16 @@
 const db2con=require('./dbCon');
 const express=require("express");
 const Promise=require("bluebird");
-const fs=require("fs");
-const fileread=Promise.promisify(fs.readFile);
-const filename=__dirname+"/data/json_file.json";
 let data={};
 const app=new express();
 
 app.get('/',(req,res)=>{
-    fileread(filename).then((chunk)=>{
-        data=JSON.parse(chunk);
-        res.send(data);
-    })
+   console.log("no parameters provided!!!");
 })
 
 app.get('/:name',(req,res)=>{
     res.header("Access-Control-Allow-Origin", "*");
     let dbresult=db2con.connect(req.params.name);
-    // res.send(JSON.stringify(dbresult));
-    /* fileread(filename).then((chunk)=>{
-        let data2=JSON.parse(chunk);
-        data=req.params;
-        data=Object.assign(data2,data);
-        data=Object.assign(data,dbresult);
-        res.send(JSON.stringify(dbresult));
-        }) */
 })
 
 app.listen(3000);
